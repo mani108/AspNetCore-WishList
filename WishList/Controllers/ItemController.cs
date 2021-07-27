4301fcfdc6da28bfb68879e6808923696d886f78
mani108 <cspuntambekar@gmail.com>
2021-07-27 19:04:59 +0530

    Code for Module 6

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -3,27 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
 using WishList.Models;
 
 namespace WishList.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View("Index", _context.Items.ToList());
         }
 
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            return View("Create");
         }
 
-        [HttpGet]
+        [HttpPost]
         public IActionResult Create(Item item)
         {
-            return View();
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.FirstOrDefault(i => i.Id == id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
